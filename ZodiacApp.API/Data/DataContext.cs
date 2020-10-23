481e5bb9e47c71c2ab694090bf9962518d8811ff
Stefon Martin <martin.stefon@gmail.com>
2020-10-23 15:11:51 -0700

    first commit

---
+++ ZodiacApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ZodiacApp.API.Models;
+
+namespace ZodiacApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values {get;set;}
+
+
+        
+    }
+}
